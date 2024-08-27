c24fa5a858d2d06f6b5515719141a72c2e40782c
Edwin <edwin.dokla.dodo@caemate.it>
2024-08-27 18:29:45 +0200

    Commit number two.

--- file-issue-nine.js
+++ file-issue-nine.js
@@ -1,1 +1,16 @@
 console.log('issue nine');
+
+
+console.log('test 1');
+console.log('test 2');
+console.log('test 7');
+console.log('test 9');
+console.log('test 6');
+console.log('test 6');
+console.log('test 4');
+console.log('test 3');
+console.log('test 3');
+
+
+
+console.log('this is the end');
